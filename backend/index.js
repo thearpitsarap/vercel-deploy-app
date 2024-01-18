2b8e9bdda97849852b720998503a5e0c8dc3796e
thearpitsarap <arpitsarap18@gmail.com>
2024-01-18 23:20:07 +0530

    removed .env locally

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const dotenv = require('dotenv');
-dotenv.config();
 const { data } = require('./db');
 
 const port = process.env.PORT;
