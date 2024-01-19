382fea19282a241e17bd9eadfb08cd62a03f8990
Arpit Sarap <73021176+thearpitsarap@users.noreply.github.com>
2024-01-19 18:22:39 +0530

    removed www

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
 }
 
 async function addData({setCompetition,setVolume,keyword}){
-  const response = await axios.get(`https://www.vercel-deploy-qzqd8077u-arpit-saraps-projects.vercel.app/?&keyword=${keyword}`);
+  const response = await axios.get(`https://vercel-deploy-qzqd8077u-arpit-saraps-projects.vercel.app/?&keyword=${keyword}`);
     console.log(response.data);
     setVolume(response.data.searchVolume);
     setCompetition(response.data.competition);
