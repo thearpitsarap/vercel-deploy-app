6a22ce889646d282f1ead003ae8bb0a74bbcd42b
Arpit Sarap <73021176+thearpitsarap@users.noreply.github.com>
2024-01-19 14:02:31 +0530

    removed headers

--- backend/index.js
+++ backend/index.js
@@ -9,15 +9,7 @@ const app = express();
 app.use(cors());
 
 app.get("/", async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
+    
     let vol, competition;
     const keyword = req.query.keyword;
     const response = await data.find({});
